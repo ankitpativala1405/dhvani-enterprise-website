829dbb82ffcb6ab7126871475130969320716f2c
ankitpativala1405 <ankitj1405@gmail.com>
2025-02-26 19:59:04 +0530

    update front end page

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 let dataStore = []; // Array to store the data before exporting
 
 document.getElementById('myForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form submission
+  event.preventDefault(); 
 
-  // Get form data
+ 
   let firstname = document.getElementById('NAME').value;
   let surname=document.getElementById('SURNAME').value;
   let contact = document.getElementById('mobile number').value;
   let scontact = document.getElementById('alternative number').value;
   let email = document.getElementById('email').value;
-  let saddress = document.getElementById('saddress').value;
+  let ad1 = document.getElementById('ad1').value;
+  let ad2 = document.getElementById('ad2').value;
+  let ad3 = document.getElementById('ad3').value;
   let landmark = document.getElementById('landmark').value;
   let pincode = document.getElementById('pincode').value;
   let city = document.getElementById('city').value;
   let state = document.getElementById('state').value;
   let password= document.getElementById('password').value;
 
-  // Store the data in an array
-  dataStore.push({ Name: name,Surname:surname,Contact:contact,alternativecontact:scontact, Email: email,address:saddress,Landmark:landmark,Pincode:pincode,City:city,State:state,Password:password });
+
+  let saddress = {
+    ad1,
+    ad2,
+    ad3,
+  };
+
+  let employeedetail={
+      Name:firstname,
+      Surname:surname,
+      Contact:contact,
+      alternativecontact:scontact,
+      Email: email,
+      address:saddress,
+      Landmark:landmark,
+      Pincode:pincode,
+      City:city,
+      State:state,
+      Password:password
+  }
+
+ 
+  dataStore.push(employeedetail);
 
   // Show download button
-  document.getElementById('downloadBtn').style.display = 'inline-block';
+  // document.getElementById('downloadBtn').style.display = 'inline-block';
 
   // Reset the form
-  document.getElementById('myForm').reset();
-});
-
-// Function to generate Excel file and download it
-document.getElementById('downloadBtn').addEventListener('click', function() {
-  // Create a workbook and add data
-  const wb = XLSX.utils.book_new();
-  const ws = XLSX.utils.json_to_sheet(dataStore); // Convert array to sheet
-  XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-  // Write the Excel file and trigger the download
-  XLSX.writeFile(wb, 'data.xlsx');
+  // document.getElementById('myForm').reset();
 });
