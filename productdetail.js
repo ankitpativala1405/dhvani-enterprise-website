1682c57e4ed7be4dada208588e478369bcb4d6f2
ankitpativala1405 <ankitj1405@gmail.com>
2025-03-21 10:01:09 +0530

    dhvani-enterprise-website

--- productdetail.js
+++ productdetail.js
@@ -12,14 +12,14 @@ let detaildata = [
     "price": "199",
     "MRP": "406",
     "category":"usb sealer machine",
-    "image1": "/PHOTO/product photo/0/1.jpg",
-    "image2": "/PHOTO/product photo/0/2.jpg", 
-    "image3": "/PHOTO/product photo/0/3.jpg", 
-    "image4": "/PHOTO/product photo/0/4.jpg",
-    "image5": "/PHOTO/product photo/0/5.jpg", 
-    "image6": "/PHOTO/product photo/0/6.jpg",
-    "image7": "/PHOTO/product photo/0/7.jpg", 
-    "image8": "/PHOTO/product photo/0/8.jpg", 
+    "image1": "./PHOTO/product photo/0/1.jpg",
+    "image2": "./PHOTO/product photo/0/2.jpg", 
+    "image3": "./PHOTO/product photo/0/3.jpg", 
+    "image4": "./PHOTO/product photo/0/4.jpg",
+    "image5": "./PHOTO/product photo/0/5.jpg", 
+    "image6": "./PHOTO/product photo/0/6.jpg",
+    "image7": "./PHOTO/product photo/0/7.jpg", 
+    "image8": "./PHOTO/product photo/0/8.jpg", 
     "bullet1": "💖 This mini bag sealer is a heat bag sealer, and also a bag opener. You can seal bags easily and keep your food fresh, also with the built-in sharp stainless steel blade to open the bags conveniently. 2-in-1 machine. Enjoy your fresh and crispy snacks.",
     "bullet2": "💖 built-in With a 400mAh battery, the rechargeable hot bond bag sealer comes with a micro USB cable(included), so there is no need to buy extra AA batteries for this chip sealer, save your money and protect the environment.",
     "bullet3": "💖 This food bag sealer is suitable for packages of chips, cookies, salad, pet foods, etc., as well as for packages of salad and vacuum bags, a must-have in daily life and kitchen cooking scenarios. Not for cling films, cellophane bags, kraft paper bags, or pure aluminum bags.",
@@ -46,14 +46,14 @@ let detaildata = [
     "price": "599", 
     "MRP": "2199", 
     "category": "Anarkali Gown", 
-    "image1": "/PHOTO/product photo/1/1.jpg", 
-    "image2": "/PHOTO/product photo/1/2.jpg", 
-    "image3": "/PHOTO/product photo/1/3.jpg", 
-    "image4": "/PHOTO/product photo/1/4.jpg", 
-    "image5": "/PHOTO/product photo/1/5.jpg", 
-    "image6": "/PHOTO/product photo/1/6.jpg", 
-    "image7": "/PHOTO/product photo/1/7.jpg", 
-    "image8": "/PHOTO/product photo/1/8.jpg", 
+    "image1": "./PHOTO/product photo/1/1.jpg", 
+    "image2": "./PHOTO/product photo/1/2.jpg", 
+    "image3": "./PHOTO/product photo/1/3.jpg", 
+    "image4": "./PHOTO/product photo/1/4.jpg", 
+    "image5": "./PHOTO/product photo/1/5.jpg", 
+    "image6": "./PHOTO/product photo/1/6.jpg", 
+    "image7": "./PHOTO/product photo/1/7.jpg", 
+    "image8": "./PHOTO/product photo/1/8.jpg", 
     "bullet1": "💖 Top: Heavy Faux Georgette With Embroidered Work With Stone, Size: Max Up To 48 Inches Length: Max Up To 52 Inches", 
     "bullet2": "💖 Bottom: Heavy Santoon, Length: 2 MTR, Type: Unstitched", 
     "bullet3": "💖 Dupatta: Heavy Butterfly Net With Embroidery Work With Latkan, Size: 2.20 MTR, Sleeve; Faux Georgette With Embroidered Work", 
@@ -92,13 +92,13 @@ let productsdata = `
                 <img src="${temporary.image1}" alt="Main product image" id="main-img">
             </div>
             <div id="thumbnails">
-                <img src="${temporary.image2}" alt="" id="image02">
-                <img src="${temporary.image3}" alt="" id="image03">
-                <img src="${temporary.image4}" alt="" id="image04">
-                <img src="${temporary.image5}" alt="" id="image05">
-                <img src="${temporary.image6}" alt="" id="image06">
-                <img src="${temporary.image7}" alt="" id="image07">
-                <img src="${temporary.image8}" alt="" id="image08">
+                <img src="${temporary.image2}" alt="" id="image02" onclick=imageswaping("image02")>
+                <img src="${temporary.image3}" alt="" id="image03" onclick=imageswaping("image03")>
+                <img src="${temporary.image4}" alt="" id="image04" onclick=imageswaping("image04")>
+                <img src="${temporary.image5}" alt="" id="image05" onclick=imageswaping("image05")>
+                <img src="${temporary.image6}" alt="" id="image06" onclick=imageswaping("image06")>
+                <img src="${temporary.image7}" alt="" id="image07" onclick=imageswaping("image07")>
+                <img src="${temporary.image8}" alt="" id="image08" onclick=imageswaping("image08")>
             </div>
         </div>
         <div id="product-info">
@@ -132,22 +132,16 @@ let productsdata = `
 
 document.getElementById("shaw-detail-here").innerHTML = productsdata;
 
-let mainImage;
-let thumbnail;
- function swapImages(thumbnailId, mainImageId) {
- 
-    let thumbnailSrc = document.getElementById(thumbnailId).src;
-    let mainImageSrc = document.getElementById(mainImageId).src;
 
-    mainImage.src = thumbnailSrc;
-    thumbnail.src = mainImageSrc;
-}
-document.getElementById("image05").addEventListener("click", ()=>{
+ const imageswaping=(id)=>{
     let mainImage = document.getElementById("main-img");
-    let thumbnail = document.getElementById("image05");
+    let thumbnail = document.getElementById(id);
 
     let mainimagesrc=mainImage.src;
     let thumbnailsrc=thumbnail.src;
 
-    swapImages("image05", "main-image"); 
-});
+      mainImage.src=thumbnailsrc;
+      thumbnail.src=mainimagesrc;
+ }
+
+
